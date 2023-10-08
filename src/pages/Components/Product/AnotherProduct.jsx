3863c8a63e2f046eefae503c75d21808502a320e
SebastienWeb <sebastienweb27@gmail.com>
2023-10-08 22:45:51 +0200

    refactor: Remove transpose function

--- src/pages/Components/Product/AnotherProduct.jsx
+++ src/pages/Components/Product/AnotherProduct.jsx
@@ -1,53 +1,41 @@
 import { Button } from "../../../components/ui/Button.jsx"
-import { transpose } from "../../../helpers/transpose.js"
 
 export function AnotherProduct({ data }) {
-    const media = [data.another_mobile, data.another_tablet, data.another_desktop]
-    // const mediaFormatted = transpose(media.map((support) => support.data))
-    // const organizedMedia = mediaFormatted.map((mediaSet) => mediaSet.sort((a, b) => a.id - b.id))
     return (
         <div className="pt-[7.5rem]">
             <h2 className="text-center text-h5 uppercase leading-9 md:text-h3">you may also like</h2>
             <div className="containerAnother flex flex-col gap-14 pt-10 md:flex-row md:gap-[0.69rem] md:pt-14 lg:gap-[1.87rem] lg:pt-16">
-                {media.map(
-                    (media, index) => console.log(media)
-                    // <div key={index}>
-                    //     <figure className=" overflow-hidden rounded-lg">
-                    //         <picture>
-                    //             <source
-                    //                 media="(min-width: 1024px)"
-                    //                 srcSet={`${import.meta.env.VITE_BASE_ADMIN}${media.data[0].attributes.url}`}
-                    //                 type={media.data[0].attributes.mime}
-                    //                 width={media.data[0].attributes.width}
-                    //                 height={media.data[0].attributes.height}
-                    //             />
-                    //             <source
-                    //                 media="(min-width: 768px) and (max-width: 1024px)"
-                    //                 srcSet={`${import.meta.env.VITE_BASE_ADMIN}${media.data[1].attributes.url}`}
-                    //                 type={media.data[1].attributes.mime}
-                    //                 width={media.data[1].attributes.width}
-                    //                 height={media.data[1].attributes.height}
-                    //             />
-                    //             <img
-                    //                 src={`${import.meta.env.VITE_BASE_ADMIN}${media.data[2].attributes.url}`}
-                    //                 alt={data.title}
-                    //                 width={media.data[2].attributes.width}
-                    //                 height={media.data[2].attributes.height}
-                    //                 loading="lazy"
-                    //                 className="w-full"
-                    //             />
-                    //         </picture>
-                    //     </figure>
-                    //     <div className="flex flex-col items-center gap-8 pt-8 lg:pt-10">
-                    //         <h3 className="text-h5 uppercase">{data.another_title[index]}</h3>
-                    //         <Button
-                    //             variant="primary"
-                    //             content="See product"
-                    //             slug={`http://localhost:5173/${data.another_slugs[index]}`}
-                    //         />
-                    //     </div>
-                    // </div>
-                )}
+                {data.other_product.data.attributes.others.map(({ slug, name, image }, index) => (
+                    <div key={index}>
+                        <figure className=" overflow-hidden rounded-lg">
+                            <picture>
+                                <source
+                                    media="(min-width: 1024px)"
+                                    srcSet={`${import.meta.env.VITE_BASE_ADMIN}${image.desktop}`}
+                                />
+                                <source
+                                    media="(min-width: 768px) and (max-width: 1024px)"
+                                    srcSet={`${import.meta.env.VITE_BASE_ADMIN}${image.tablet}`}
+                                />
+                                <img
+                                    src={`${import.meta.env.VITE_BASE_ADMIN}${image.mobile}`}
+                                    alt={name}
+                                    loading="lazy"
+                                    className="w-full"
+                                />
+                            </picture>
+                        </figure>
+                        <div className="flex flex-col items-center gap-8 pt-8 lg:pt-10">
+                            <h3 className="text-h5 uppercase">{name}</h3>
+                            <Button
+                                variant="primary"
+                                content="See product"
+                                slug={`http://localhost:5173/${slug}`}
+                                onclick={window.scrollTo({ top: 0, left: 0, behavior: "smooth" })}
+                            />
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
     )
--- src/pages/Components/Product/Gallery.jsx
+++ src/pages/Components/Product/Gallery.jsx
@@ -1,32 +1,16 @@
-import { transpose } from "../../../helpers/transpose.js"
-
 export function Gallery({ data }) {
-    const media = [data.preview_desktop, data.preview_tablet, data.preview_mobile]
-    const mediaFormatted = transpose(media.map((support) => support.data))
-
     return (
         <div className="containerGrid grid grid-cols-1 gap-5 pt-[5.5rem] md:grid-cols-2 md:grid-rows-2 md:pt-[7.5rem] lg:gap-[1.875rem]">
-            {mediaFormatted.map((media, index) => (
+            {data.gallery.map(({ desktop, tablet, mobile }, index) => (
                 <picture key={index} className="block overflow-hidden rounded-lg">
-                    <source
-                        media="(min-width: 1024px)"
-                        srcSet={`${import.meta.env.VITE_BASE_ADMIN}${media[0].attributes.url}`}
-                        type={media[0].attributes.mime}
-                        width={media[0].attributes.width}
-                        height={media[0].attributes.height}
-                    />
+                    <source media="(min-width: 1024px)" srcSet={`${import.meta.env.VITE_BASE_ADMIN}${desktop}`} />
                     <source
                         media="(min-width: 768px) and (max-width: 1024px)"
-                        srcSet={`${import.meta.env.VITE_BASE_ADMIN}${media[1].attributes.url}`}
-                        type={media[1].attributes.mime}
-                        width={media[1].attributes.width}
-                        height={media[1].attributes.height}
+                        srcSet={`${import.meta.env.VITE_BASE_ADMIN}${tablet}`}
                     />
                     <img
-                        src={`${import.meta.env.VITE_BASE_ADMIN}${media[2].attributes.url}`}
+                        src={`${import.meta.env.VITE_BASE_ADMIN}${mobile}`}
                         alt={data.title}
-                        width={media[2].attributes.width}
-                        height={media[2].attributes.height}
                         loading="lazy"
                         className="h-full w-full object-cover"
                     />
