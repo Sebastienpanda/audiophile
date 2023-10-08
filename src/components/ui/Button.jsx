db4ce4a80ceb343e7a27617a44e57a3d7b695d18
SebastienWeb <sebastienweb27@gmail.com>
2023-10-08 22:36:54 +0200

    feat: Add onClick event

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -4,13 +4,14 @@ const variantClasses = {
     primary: "bg-primary text-white hover:bg-hoverBtn",
 }
 
-export function Button({ content, style, variant, slug }) {
+export function Button({ content, style, variant, slug, onclick }) {
     const variantClass = variantClasses[variant]
     return (
         <div className="flex justify-center lg:justify-start">
             <Link
                 to={slug}
                 className={`btn ${style} text-shop uppercase transition-colors duration-[350ms] ease-in-out ${variantClass}`}
+                onClick={onclick}
             >
                 {content}
             </Link>
